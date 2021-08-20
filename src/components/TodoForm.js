061abf2ad16f0d16f16ab5f5d2f9c155a2fbcb8b
sudeepac <sudeepac@gmail.com>
2021-08-20 18:36:20 +1000

    Create Form for Todo

---
+++ src/components/TodoForm.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+export const TodoForm = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-group">
+          <label htmlFor="text">Text:</label>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="title"
+            {...register("title", { required: true })}
+          />
+
+          <label htmlFor="description">Description:</label>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="description"
+            {...register("description", { required: true })}
+          />
+
+          <label htmlFor="status">Status:</label>
+          <select
+            className="form-control"
+            {...register("status", { required: true })}
+          >
+            <option value="open">open</option>
+            <option value=" in progress"> in progress</option>
+            <option value=" closed"> closed</option>
+          </select>
+        </div>
+
+        <div className="form-group">
+          <button type="submit" className="btn btn-primary">
+            Save Todo
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
--- src/pages/CreateTodo.js
+++ src/pages/CreateTodo.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { TodoForm } from "../components/TodoForm";
+// import { createTodo } from "./api";
+// import { useHistory } from "react-router-dom";
 
 export const CreateTodo = () => {
+  //   const history = useHistory();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    // history.push("/");
+  };
+
+  //   const onSubmit = async (data) => {
+  //     await createTodo(data);
+  //     history.push("/");
+  //   };
+
   return (
-    <div>
-      <p>Create Todo Page</p>
+    <div className="container">
+      <div className="mt-3">
+        <h3>Create Todo Item</h3>
+        <TodoForm onSubmit={onSubmit} />
+      </div>
     </div>
   );
 };
