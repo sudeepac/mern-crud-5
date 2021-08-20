9441532a323a0b0eff91274d3df7ac2306f610f9
sudeepac <sudeepac@gmail.com>
2021-08-20 19:58:26 +1000

    Create a backend server

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/todos5DB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", () => {
+  console.log("Mongodb connection established successfully");
+});
+
+const PORT = 4000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
